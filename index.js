ee95d200cb95d9a06c82fe01fc39a999417fbdd3
Robbie McCorkell <robbiemccorkell@gmail.com>
2017-10-02 15:15:37 +0100

    basic ES client

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-module.exports = () => 'Hello World'
+const elasticsearch = require('elasticsearch');
+
+const { DATABASE_SERVICE_NAME } = process.env;
+
+module.exports = (req, res) => {
+  const client = new elasticsearch.Client({
+    host: `${DATABASE_SERVICE_NAME}:9200`,
+    log: 'trace',
+  });
+
+  client
+    .ping({
+      requestTimeout: 30000,
+    })
+    .then(
+      function(body) {
+        return res.end('Found ES');
+      },
+      function(error) {
+        return res.end("Couldn't find ES");
+      },
+    );
+};
