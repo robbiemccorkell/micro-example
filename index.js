2eadd60c3ec6f952133418b9ace4060c8303de99
Robbie McCorkell <robbiemccorkell@gmail.com>
2017-10-02 15:34:16 +0100

    add service and host env vars

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const elasticsearch = require('elasticsearch');
 
-const { ELASTICSEARCH_DATABASE, ELASTICSEARCH_SERVICE_HOST } = process.env;
+const { ELASTICSEARCH_DATABASE, ELASTICSEARCH_SERVICE_HOST, ELASTICSEARCH_SERVICE_PORT } = process.env;
 
 module.exports = (req, res) => {
   console.log(ELASTICSEARCH_DATABASE);
   console.log(ELASTICSEARCH_SERVICE_HOST);
+  console.log(ELASTICSEARCH_SERVICE_PORT);
+  console.log(ELASTICSEARCH_SERVICE_PORT);
   const client = new elasticsearch.Client({
-    host: `${ELASTICSEARCH_DATABASE}:9200`,
+    host: `${ELASTICSEARCH_SERVICE_HOST}:${ELASTICSEARCH_SERVICE_PORT}`,
     log: 'trace',
   });
 
